d57fd1bce9e793388debd9ebd12f6db076729b82
Eldar Abdivaitov <eldarmeta@outlook.com>
2025-10-16 10:24:26 -0400

    Add Transaction model class with CSV and toString formatting (2)

--- src/main/java/com.eldar.ledger/Transaction.java
+++ src/main/java/com.eldar.ledger/Transaction.java
@@ -1,4 +1,75 @@
 package com.eldar.ledger;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
 public class Transaction {
+    private LocalDate date;
+    private LocalTime time;
+    private String description;
+    private String vendor;
+    private double amount;
+
+    public Transaction(LocalDate date, LocalTime time, String description, String vendor, double amount) {
+        this.date = date;
+        this.time = time;
+        this.description = description;
+        this.vendor = vendor;
+        this.amount = amount;
+    }
+
+    public String toCSV() {
+        return date + "|" + time + "|" + description + "|" + vendor + "|" + amount;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s %s | %-20s | %-15s | %10.2f",
+                date.format(DateTimeFormatter.ISO_DATE),
+                time.format(DateTimeFormatter.ofPattern("HH:mm:ss")),
+                description,
+                vendor,
+                amount);
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public LocalTime getTime() {
+        return time;
+    }
+
+    public void setTime(LocalTime time) {
+        this.time = time;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getVendor() {
+        return vendor;
+    }
+
+    public void setVendor(String vendor) {
+        this.vendor = vendor;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
 }
